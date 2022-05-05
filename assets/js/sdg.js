37b4678b4ec85746ce4583eb46d625cf30ffaa07
Viciano <94388326+Viciano@users.noreply.github.com>
2022-05-05 08:03:25 +0200

    Update sdg.js

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2,7 +2,7 @@
 # Don't delete this line.
 ---
 #{%- include assets/js/autotrack-element.js -%}
-#{%- include assets/js/plugins/jquery.sdgMap.js -%}
+{%- include assets/js/plugins/jquery.sdgMap.js -%}
 #{%- include assets/js/chartjs/rescaler.js -%}
 #{%- include assets/js/chartjs/noDataMessage.js -%}
 #{% if site.accessible_charts %}
